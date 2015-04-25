dbc3801d5778a9ee23516ad10608ed36f72d497d
rourkke <djalmondy@gmail.com>
2015-04-25 12:28:10 -0400

    HW assignment 2-2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 ## the functions support computing a matrix's inverse and storing
 ## the data so that it does not need to be recomputed as long as you are 
 ## using the same matrix
 
-## Write a short comment describing this function
 ## Object to store a matrix and its inverse
 ## Args :
 ##      x: the matrix to be stored
